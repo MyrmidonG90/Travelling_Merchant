dc4f855657503ea43761eb000d0f38b6ea8b4062
MyrmidonG90 <31126152+MyrmidonG90@users.noreply.github.com>
2020-03-26 10:40:13 +0100

    initial project commit

---
+++ Active/WorldModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Active
+{
+    class WorldModule
+    {
+        Texture2D texMap;
+
+        public WorldModule(Texture2D tex)
+        {
+            texMap = tex;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texMap, Vector2.Zero, Color.White);
+        }
+    }
+}
