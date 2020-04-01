af9483b6ee2497498727314fb7a36384a39210ce
Knasse01 <62263665+Knasse01@users.noreply.github.com>
2020-04-01 13:15:24 +0200

    KMReader

---
+++ Active/KMReader.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Active
+{
+    static class KMReader
+    {
+        public static KeyboardState keyState, prevKeyState;
+        public static MouseState mouseState, prevMouseState;
+
+        public static void Update()
+        {
+            if (prevMouseState != null)
+            {
+                prevMouseState = mouseState;
+            }
+            if (prevKeyState != null)
+            {
+                prevKeyState = keyState;
+            }
+            keyState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+        }
+        public static Vector2 GetMouseVector2()
+        {
+            return new Vector2(mouseState.Position.X, mouseState.Position.Y);
+        }
+        public static Point GetMousePoint()
+        {
+            return mouseState.Position;
+        }
+        public static bool MouseClick()
+        {
+            if (prevMouseState.LeftButton == ButtonState.Released && mouseState.LeftButton == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+}
