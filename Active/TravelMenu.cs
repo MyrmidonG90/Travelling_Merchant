b58919ef43d1aa82c7dbdfd79c2830f8d5668885
FridaStenfeldt <fstenfeldt@gmail.com>
2020-04-09 16:49:41 +0200

    Update TravelMenu.cs

--- Active/TravelMenu.cs
+++ Active/TravelMenu.cs
@@ -21,19 +21,22 @@ namespace Active
             turnsToTravel = 5;
             currentTurnTickDown = turnsToTravel;
             turnTimer = 3;
+
+            turnDisplay = currentTurnTickDown.ToString() + "/" + turnsToTravel.ToString();
         }
 
         public void Update(GameTime gameTime)
         {
             turnDisplay = currentTurnTickDown.ToString() + "/" + turnsToTravel.ToString();
 
-            if(turnTimer >= 1)
+            if(turnTimer > 0)
             {
                 turnTimer = turnTimer - gameTime.ElapsedGameTime.TotalSeconds;
             }
 
             if(turnTimer <= 0)
             {
+                currentTurnTickDown--;
                 turnTimer = 3;
             }
         }
@@ -41,6 +44,7 @@ namespace Active
         public void Draw (SpriteBatch spriteBatch)
         {
             spriteBatch.DrawString(TextureManager.font, turnDisplay, new Vector2(50, 50), Color.White);
+            spriteBatch.DrawString(TextureManager.font, " " + turnTimer, new Vector2(50, 150), Color.White);
         }
     }
 }
