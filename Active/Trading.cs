f0b20e7b619b8efc2d49a0bcee904dd4caf4946c
Knasse01 <62263665+Knasse01@users.noreply.github.com>
2020-04-10 16:32:13 +0200

    Update Trading.cs

--- Active/Trading.cs
+++ Active/Trading.cs
@@ -8,22 +8,22 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 namespace Active
 {
-    public static class Trading
+    static class Trading
     {
 
-        static Inventory invLeft, invRight,tradeLeft,tradeRight;
-        static  Slot[,] slotsLeft, slotsRight,tradeSlotsLeft,tradeSlotsRight;
-        static Button accept, reset,back;
+        static Inventory invLeft, invRight, tradeLeft, tradeRight;
+        static Slot[,] slotsLeft, slotsRight, tradeSlotsLeft, tradeSlotsRight;
+        static Button accept, reset, back;
         enum Participant
         {
             Left,
             Right,
             None
         }
-       static int counterCol, counterRow,tmpCounter,leftPrice,rightPrice,priceDifference;
+        static int counterCol, counterRow, tmpCounter, leftPrice, rightPrice, priceDifference;
 
         // Klart
-        static void Initialize(Inventory left, Inventory right)
+        static public void Initialize(Inventory left, Inventory right)
         {
             invLeft = left;
             invRight = right;
@@ -43,24 +43,24 @@ namespace Active
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
-                {                    
-                    slotsLeft[i, j] = new Slot(50+60*j,100+60*i,50,50);
-                    slotsRight[i, j] = new Slot(1570+60*j,100+60*i,50,50);
+                {
+                    slotsLeft[i, j] = new Slot(50 + 60 * j, 100 + 60 * i, 50, 50);
+                    slotsRight[i, j] = new Slot(1570 + 60 * j, 100 + 60 * i, 50, 50);
                 }
             }
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
-                {                    
-                    tradeSlotsLeft[i,j] = new Slot(1010+60*j,100+60*i,50,50);
-                    tradeSlotsRight[i, j] = new Slot(660+60*j,100+60*i,50,50);
+                {
+                    tradeSlotsLeft[i, j] = new Slot(1010 + 60 * j, 100 + 60 * i, 50, 50);
+                    tradeSlotsRight[i, j] = new Slot(660 + 60 * j, 100 + 60 * i, 50, 50);
                 }
             }
         }
 
         // Klar
         static void CreateButtons() // Initialiserar knapparna
-        {            
+        {
             accept = new Button(710, 630, 500, 200, TextureManager.texBox);
             reset = new Button(1260, 630, 500, 200, TextureManager.texBox);
             back = new Button(160, 630, 500, 200, TextureManager.texBox);
@@ -68,7 +68,7 @@ namespace Active
 
         // Håller processen trading igång. När den returnerar false betyder det att transaktionen är ej över och vice versa när man returnerar true
         // Klar?
-        static bool Update(ref Inventory participantLeft, ref Inventory participantRight)
+        static public bool Update(ref Inventory participantLeft, ref Inventory participantRight)
         {
             //När ett mussklick händer
             if (KMReader.MouseClick())
@@ -94,13 +94,14 @@ namespace Active
                 // Checkar om man har klickat på inventory:n
                 else
                 {
-                    if (CheckInvSlotClick(tradeSlotsLeft, Participant.Left) == false)
+                    if (CheckInvSlotClick(slotsLeft, Participant.Left) == false)
                     {
-                        CheckInvSlotClick(tradeSlotsLeft, Participant.Right);
+                        CheckInvSlotClick(slotsRight, Participant.Right);
                     }
-                }                
+                    // glömde lägga till 
+                }
             }
-            
+
             return false;
         }
 
@@ -118,32 +119,32 @@ namespace Active
                     {
                         ++counterRow; // Ökar counterRow
                     }
-                } while (counterRow < 5 && slots[counterCol, counterRow].Clicked() == false);
+                } while (counterRow < 3 && slots[counterCol, counterRow].Clicked() == false);
 
                 if (slots[counterCol, counterRow].Clicked() == false)
                 {
                     ++counterCol; // Ökar counterCol
                 }
-            } while (counterCol < 5 && slots[counterCol, counterRow].Clicked() == false);
+            } while (counterCol < 4 && slots[counterCol, counterRow].Clicked() == false);
 
             if (slots[counterCol, counterRow].Clicked() == true) // Om Slots:en som tillhör inventory:n har blivit klickat
             {
                 if (participant == Participant.Left)
                 {
-                    tradeLeft.AddItem(slots[counterCol, counterRow].Item.ID,1);// Lägger till item till det vänstra trade fältet   
+                    tradeLeft.AddItem(slots[counterCol, counterRow].Item.ID, 1);// Lägger till item till det vänstra trade fältet   // Error Finns inte
                     invLeft.ReduceAmountOfItems(slots[counterCol, counterRow].Item.ID, 1);
                 }
                 else if (participant == Participant.Right)
                 {
                     tradeRight.AddItem(slots[counterCol, counterRow].Item.ID, 1); // Lägger till item till det högra trade fältet
-                    invRight.ReduceAmountOfItems(slots[counterCol, counterRow].Item.ID,1);
+                    invRight.ReduceAmountOfItems(slots[counterCol, counterRow].Item.ID, 1);
                 }
 
                 UpdateSlots();
                 UpdatePrices();
                 return true;
             }
-            
+
 
             return false;
         }
@@ -303,7 +304,7 @@ namespace Active
                     catch (Exception) // Ifall det finns tomma rutor
                     {
                         tradeSlotsRight[i, j].Item = null;
-                    }                    
+                    }
                 }
             }
             for (int i = 0; i < 5; i++)
@@ -332,7 +333,7 @@ namespace Active
                     {
                         slotsRight[i, j].Item = null;
                     }
-                    
+
                 }
             }
         }
@@ -349,7 +350,7 @@ namespace Active
         static bool AcceptTrade(ref Inventory participantLeft, ref Inventory participantRight)
         {
             //If nothing is presented
-            if (tradeLeft.ItemList.Count == 0 && tradeRight.ItemList.Count==0)
+            if (tradeLeft.ItemList.Count == 0 && tradeRight.ItemList.Count == 0)// !!!Något fel här!!!
             {
                 return false;
             }
@@ -359,7 +360,7 @@ namespace Active
             {
                 return false;
             }
-            
+
             //If player doesn't have enough coin and merchant neither.
             //Else prompt that you do not have enough gold
             if (priceDifference != 0)
@@ -402,7 +403,7 @@ namespace Active
                 {
                     try
                     {
-                        slotsLeft[j, i].Item = invLeft.ItemList[i * 5 + j];                        
+                        slotsLeft[j, i].Item = invLeft.ItemList[i * 5 + j];
                     }
                     catch (Exception)
                     {
@@ -416,7 +417,7 @@ namespace Active
                     {
                         slotsRight[j, i].Item = null;
                     }
-                    
+
                 }
             }
 
@@ -455,8 +456,8 @@ namespace Active
             tradeRight = null;
         }
 
-        // Klar
-        static void Draw(SpriteBatch sb)
+        // Ej klar, måste införa antal pengar och sånt
+        static public void Draw(SpriteBatch sb)
         {
             foreach (var item in slotsLeft)
             {
@@ -478,5 +479,5 @@ namespace Active
             reset.Draw(sb);
             back.Draw(sb);
         }
-    }     
+    }
 }
