41915b2ec31172bb85bfe7d070b0cf5f4247048a
Knasse01 <62263665+Knasse01@users.noreply.github.com>
2020-04-16 14:09:39 +0200

    Update Trading.cs
    Minskade antal rader för metoden UpdateSlots()

--- Active/Trading.cs
+++ Active/Trading.cs
@@ -151,12 +151,6 @@ namespace Active
                     {
                         tradeSlotsLeft[i, j].Item = null;
                     }
-                }
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
                     try
                     {
                         tradeSlotsRight[i, j].Item = tradeRight.ItemList[i * 3 + j];
@@ -179,12 +173,6 @@ namespace Active
                     {
                         slotsLeft[i, j].Item = null;
                     }
-                }
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
                     try
                     {
                         slotsRight[i, j].Item = invRight.ItemList[i * 5 + j];
@@ -193,7 +181,6 @@ namespace Active
                     {
                         slotsRight[i, j].Item = null;
                     }
-
                 }
             }
         }
