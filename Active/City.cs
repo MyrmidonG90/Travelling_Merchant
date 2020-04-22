041ec66e32d4ed25de3a74826a30303a816c36c0
Knasse01 <62263665+Knasse01@users.noreply.github.com>
2020-04-22 16:00:21 +0200

    Fixing inventory for cities

--- Active/City.cs
+++ Active/City.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+using System.Windows.Forms;
+using System.Reflection;
 
 namespace Active
 {
@@ -13,14 +18,20 @@ namespace Active
         string name;
         string information;
         Vector2 coordinates;
+        Inventory inv;
+        StreamReader sr;
+        StreamWriter sw;
+        float x, y;
 
         public City(string name, string information, Vector2 coordinates)
         {
             this.name = name;
             this.information = information;
             this.coordinates = coordinates;
-
+            x = coordinates.X;
+            y = coordinates.Y;
         }
+        
 
         public string Name
         {
@@ -28,6 +39,32 @@ namespace Active
             {
                 return name;
             }
+        }
+        /*File structure
+         * name;information;x;y;money;inventory 
+         * Inventory structure
+         * ;item1:amount,item2:amount,item3:amount
+         * 
+         */
+
+        void LoadCity()
+        {
+            
+            FileManager.ReadFile("");
+            
+            FileManager.SplitText('h', FileManager.fileText);
+
+            //Ladda in fil
+            //Sök efter stadens namnet
+            //Läs in datan i en string
+            //Splitta datan korrekt
+            //Sätt rätt data till respektive stad
+
+
+        }
+        public void AddInventory(Inventory inv)
+        {
+            this.inv = inv;
         }
 
         public string Information
@@ -49,5 +86,7 @@ namespace Active
                 return coordinates;
             }
         }
+
+        public Inventory Inv { get => inv; set => inv = value; }
     }
 }
---
+++ Active/FileManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Xna.Framework;
+namespace Active
+{
+    static class FileManager
+    {
+        static public StreamReader sr;
+        static public StreamWriter sw;
+        static public FileStream fs;
+        static public string[] charSplitter;
+        static public string[] secondCharSplitter;
+        static public string[] thirdCharSplitter;
+        static public string fileText;
+        static public List<string> readFilePerLine;
+        static public List<City> cities;
+
+        public static string[] SplitText(char splitChar, string text)
+        {
+            string[] splitter;
+            splitter = text.Split(splitChar);
+            return splitter;
+        }
+        public static void ReadFile(string name)
+        {
+            fileText = null;
+            if (File.Exists(name))
+            {
+                sr = new StreamReader(name);
+                sr.ReadToEnd();
+            }
+        }
+        public static void ReWriteFile(string name, string text)
+        {
+            if (File.Exists(name))
+            {
+                EmptyFile(name);
+                sw = new StreamWriter(name);
+                sw.Write(text);
+                sw.Close();
+            }            
+        }
+        public static void ReWriteFile(string name, List<string> text)
+        {
+            if (File.Exists(name))
+            {
+                EmptyFile(name);
+                sw = new StreamWriter(name);
+                foreach (var item in text)
+                {
+                    sw.WriteLine(item);
+                }
+                sw.Close();
+            }
+        }
+        public static void CreateCities()
+        {
+            int nrOfTerms = 5;
+            ReadFile("");
+            
+            charSplitter = SplitText(';', fileText);
+            
+            for (int i = 0; i < charSplitter.Length/nrOfTerms; i++)
+            {
+                cities.Add(new City(charSplitter[nrOfTerms*i%nrOfTerms],charSplitter[nrOfTerms*(i+1)%nrOfTerms],new Vector2(float.Parse(charSplitter[nrOfTerms * (i + 2) % nrOfTerms]), float.Parse(charSplitter[nrOfTerms * (i + 3) % nrOfTerms]))));
+                secondCharSplitter = SplitText(',', charSplitter[nrOfTerms*(5+i)%nrOfTerms]);
+                Inventory inv = new Inventory(int.Parse(charSplitter[nrOfTerms*(4+i)%nrOfTerms]));
+                for (int j = 0; j < secondCharSplitter.Length; j++)
+                {
+                    thirdCharSplitter = SplitText(':', secondCharSplitter[j]);
+                    inv.AddItem(int.Parse(thirdCharSplitter[0]), int.Parse(thirdCharSplitter[1]));
+                    
+                }
+                cities[i / nrOfTerms].AddInventory(inv);
+            }
+        }
+        static void Reset()
+        {
+            fileText = null;
+            charSplitter = null;
+            secondCharSplitter = null;
+            thirdCharSplitter = null;
+        }
+
+        public static void EmptyFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, String.Empty);
+            }            
+        }
+        
+    }
+}
